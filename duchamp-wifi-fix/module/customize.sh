f03909e355f60c3310f9f89cc5f41a7014bb66a3
Zaid.nvr <utegfer@gmail.com>
2026-08-06 12:30:42 -0600

    wifi v2: install early compatibility components

--- duchamp-wifi-fix/module/customize.sh
+++ duchamp-wifi-fix/module/customize.sh
@@ -4,11 +4,15 @@ ui_print "***************************************"
 ui_print " POCO X6 Pro Wi-Fi WPA2 Compatibility "
 ui_print "***************************************"
 ui_print "- Instalando RRO estatico para Wi-Fi"
-ui_print "- Objetivo: com.android.wifi.resources"
-ui_print "- Reinicia y vuelve a guardar la red Wi-Fi"
+ui_print "- Instalando correccion local temprana WPA2"
+ui_print "- No muestra ni sube credenciales"
+ui_print "- No olvides la red antes del primer reinicio"
+ui_print "- Reinicia Android para aplicar la correccion"
 
 if [ "${API:-0}" -lt 30 ]; then
   abort "! Android 11 o superior es obligatorio"
 fi
 
 set_perm_recursive "$MODPATH/system" 0 0 0755 0644
+set_perm "$MODPATH/post-fs-data.sh" 0 0 0755
+set_perm "$MODPATH/bin/wifi_psk_compat" 0 0 0755
